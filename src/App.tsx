4b69816bc5cb7cf7c52c4b804d775030538f9714
Sif1 <soutogu@hotmail.com>
2021-12-19 11:29:04 -0300

    removing some unused vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from './pages/HomeScreen';
 import UserScreen from './pages/UserScreen';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/:id" element={<UserScreen />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
